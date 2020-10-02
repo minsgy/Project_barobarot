558390ec6e4197f1d8b5b0c0529ac8409d050576
minsgy <alstjrchl97@likelion.org>
2020-10-03 03:59:49 +0900

    [minseok] : engineers DetailView, ListView 구현, 엔지니어 detail 페이지 추가 및 list 페이지 변경

--- engineers/views.py
+++ engineers/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from . import models
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 # Create your views here.
 
 class ListEngineer(ListView): # ListView를 이용한 기사 나열
@@ -8,3 +8,9 @@ class ListEngineer(ListView): # ListView를 이용한 기사 나열
     model = models.Engineer
     context_object_name = 'engineers' # 객체를 부르는 이름
     template_name = 'engineers/engineers_list.html' # Default 연결 값 변경
+
+class DetailEngineer(DetailView): # DetailView 리스트 값을 전부 반환함.
+
+    model = models.Engineer
+    context_object_name = 'engineers' # 객체를 부르는 이름
+    template_name = 'engineers/engineers_detail.html' # Default 연결 값 변경    
