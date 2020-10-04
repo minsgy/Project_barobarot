ab423a5c3410029e3e3afbf179e150044113429f
minsgy <alstjrchl97@naver.com>
2020-10-04 16:56:20 +0900

    [minseok] : config.url (comment 연결) , 각 엔지니어마다 comment 추가함, create 기능 구현 중 미완성.

--- comments/urls.py
+++ comments/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-urlpatterns = [
-    # path('', views.<>, name="<>"),
 
+# app_name = comments
+urlpatterns = [
+    path('', views.createComment, name='create_comment'),
 ]
--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Comment
 # Create your views here.
+
+def createComment(request):
+    print(request)
+    # if request.method
+    # title = request.POST.get('title')
+    # content = request.POST.get('content')
+    return render(request, 'home.html')
+
