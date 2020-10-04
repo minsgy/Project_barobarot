4ee10d09d984924f95b54388b0fecd39c3d4a2b0
minsgy <alstjrchl97@naver.com>
2020-10-04 15:49:09 +0900

    [minseok] : comments model, admin - user FK 추가,

--- comments/models.py
+++ comments/models.py
@@ -4,5 +4,5 @@ class Comment(models.Model):
     title = models.CharField(max_length=20) # 댓글 제목
     content = models.TextField() # 댓글 내용
     engineer = models.ForeignKey("engineers.Engineer", related_name="comments", on_delete=models.CASCADE) # 연결된 기사
-    # user = models.ForeignKey("users.User", related_name="comments", on_delete=models.CASCADE) # 댓글 작성자
+    user = models.ForeignKey("users.User", related_name="comments", on_delete=models.CASCADE) # 댓글 작성자
     
