36740b6079c6a1b2536893c906b2cf89d6ed0f26
minsgy <alstjrchl97@naver.com>
2020-10-04 17:50:06 +0900

    [minseok] : 댓글 Create 구현 (No Form), templates 에 적용!!

--- comments/views.py
+++ comments/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, redirect
 from .models import Comment
+from engineers.models import Engineer
+from users.models import User
 # Create your views here.
 
-def createComment(request):
-    print(request)
-    # if request.method
-    # title = request.POST.get('title')
-    # content = request.POST.get('content')
-    return render(request, 'home.html')
+def createComment(request, cm_pk, user_pk):
+
+    engineer = Engineer.objects.get(pk=cm_pk)
+    user = User.objects.get(pk=user_pk)
+    if request.method == 'POST':
+        comment = Comment()
+        comment.engineer = engineer
+        comment.user = user
+        comment.title = request.POST.get('title')
+        comment.content = request.POST.get('content')
+        comment.save()
+        return redirect('')
+
 
