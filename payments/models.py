7f59d49378c3d7c01ca5c0f1cfda97a7a92da6fe
minsgy <alstjrchl97@naver.com>
2020-10-07 13:42:09 +0900

    [minseok] payment - model 추가 Receiver(수령자 정보), products price 필드 속성 변경

--- payments/models.py
+++ payments/models.py
@@ -4,6 +4,9 @@ from core import models as core_models
 # Creater - minseok 
 
 class Paymentproduct(core_models.TimeStampedModel):
+
+   ''' 사용자 배달 주문 아이템 '''
+
    product = models.ForeignKey('products.Product', related_name='payments',on_delete=models.CASCADE) # 연결 된 상품
    user = models.ForeignKey('users.User', related_name='payments', on_delete=models.CASCADE) # 주문한 사용자
    engineer = models.ForeignKey('engineers.Engineer',related_name='payments' ,on_delete=models.CASCADE) # 연결 된 설치기사
@@ -17,3 +20,19 @@ class Paymentproduct(core_models.TimeStampedModel):
    def __str__(self):
       return f"{self.user} - {self.engineer}"
           
+
+class Receiver(models.Model):
+
+   ''' 주문 아이템 - 받는 사람 정보'''
+
+   paymentproduct = models.OneToOneField('Paymentproduct', on_delete=models.CASCADE) 
+   # 주문 아이템과 1:1 관계, 주문 아이템과 연결 된 배달 정보
+
+   receiver = models.CharField(max_length=10) # 받는 사람 이름
+   receiver_number = models.CharField(max_length=15) # 받는 사람 연락처
+   receiver_plus_number = models.CharField(max_length=15) # 받는 사람 추가 연락처
+   zip_code = models.PositiveIntegerField(default=0) # 우편 번호
+   address = models.CharField(max_length=30) # 주소
+   delivery_message = models.CharField(max_length=50) # 배송 메세지
+
+   
--- products/models.py
+++ products/models.py
@@ -1,16 +1,17 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 #[형민] Product 모델
+#[minseok] 10. 07 - 필드 변경 ( rating - floatfield )
 
 class Product(models.Model):
 
     ''' Product model '''
 
     product_name = models.CharField(max_length=50)
-    sale = models.FloatField()                          #할인율
-    price = models.IntegerField()
+    sale = models.FloatField() #할인율
+    price = models.PositiveIntegerField(default=0, blank=True)
     manufacture = models.CharField(max_length=50)     #제조사
-    rating = models.DecimalField(default=0.0, max_digits=2, decimal_places=1)        #최대 평점 9.9
+    rating = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(5.0)])  #최소/최대 평점 0.0 ~ 5.0
     product_image = models.ImageField(upload_to="product/product_image")
     #상품 상세설명
     image_description = models.ImageField(upload_to="product/image_description")
