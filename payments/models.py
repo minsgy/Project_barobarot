1f524fb092d3c55ecd3fba93a81a3ee3ef6828aa
minsgy <alstjrchl97@likelion.org>
2020-10-08 21:25:16 +0900

    [minseok] : payments model null true

--- payments/models.py
+++ payments/models.py
@@ -14,7 +14,7 @@ class Paymentproduct(core_models.TimeStampedModel):
    engineer = models.ForeignKey('engineers.Engineer',related_name='payments' ,on_delete=models.CASCADE) # 연결 된 설치기사
   
    amount = models.PositiveIntegerField(default=0) # 물건 수량 , product의 price와 사칙연산 예정(*)
-   address = models.CharField(max_length=30) # 주소 검색 API 구현
+   address = models.CharField(max_length=30, null=True) # 주소 검색 API 구현
    
    created_time = core_models.TimeStampedModel.created # 주문 시간 - 객체 생성 시간
    visit_date = models.DateTimeField() # 방문 날짜 및 시간
