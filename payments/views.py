e4e8b49369b697bf1cab2cba9e6b020091286aff
thalals <thalals97@gmail.com>
2020-10-08 19:04:01 +0900

    [hmin] 하이마트로고 홈 연결, paymentproduct 객체 생성 test

--- payments/views.py
+++ payments/views.py
@@ -1,5 +1,6 @@
 from django.http import request
 from django.views.generic.base import TemplateView
+import engineers
 from payments.models import Paymentproduct, core_models
 from django.db import models
 from django.shortcuts import get_object_or_404, redirect, render
@@ -42,7 +43,7 @@ class payment_do(DetailView):
         # url은 `path('<pk>/', PhotoView.as_view())으로 구현되어있어서 해당 pk 부분을 받아와야함
 
         context['paymentproducts'] = models.Paymentproduct
-        context['engineers'] = engineer_model.Engineer
+        context['engineers'] = engineer_model.Engineer.objects.all()        #모든 엔지니어 데이터 넘기기
         return context
 
         
@@ -53,7 +54,9 @@ def PaymentproductCreate(request, product_pk, user_pk):
 
     products_data = product_model.Product.objects.get(pk=product_pk)
     user_data = user_model.User.objects.get(pk=user_pk)
-    # engineer_data = engineer_model.Engineer.objects.get(pk=engineer_pk)   #엔지니어 연결
+    engineer_data = engineer_model.Engineer.objects.get(pk=(request.POST.get('engineer')))   #엔지니어 연결
+
+    print("엔지니어 pk : ",request.POST.get('engineer'))
 
     paymentproducts = get_object_or_404(Paymentproduct)
 
@@ -61,15 +64,15 @@ def PaymentproductCreate(request, product_pk, user_pk):
 
         paymentproducts.product = products_data
         paymentproducts.user = user_data
-        # paymentproducts.engineer = engineer_data
+        paymentproducts.engineer = engineer_data
 
         paymentproducts.amount = request.POST.get('amount')
         paymentproducts.address = request.POST.get('adress')
 
-        paymentproducts.created_time = core_models.TimeStampedModel.created
-        paymentproducts.visit_date = core_models.TimeStampedModel.created
+        # paymentproducts.created_time = core_models.TimeStampedModel.created
+        paymentproducts.visit_date = request.POST.get('visit')
  
         paymentproducts.save()
-        return redirect('')
+        return redirect('home')
 
     return render(request, 'payments/payment_doing.html', {'paymentproducts' : paymentproducts})
