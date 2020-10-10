384947cd52f9cbdd7f3f31ba20715efd0f303be9
minsgy <alstjrchl97@naver.com>
2020-10-10 17:00:24 +0900

    [minseok] : payment url, view order_success 함수 추가 및 order_success 페이지 추가, Lpoint, 주문 코드 출력 완료

--- payments/urls.py
+++ payments/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     #<int:engineer_pk> 연결 필요
     path('paymentproduct/create/<int:product_pk>/<int:user_pk>/', views.PaymentproductCreate, name="paymentproduct_create"),
     path('paymentproduct/<int:pk>/',views.payment_do.as_view(), name = "payment_do"),
-
+    path('order_success/<int:user_pk>/<int:payment_pk>/', views.order_success, name="order_success")
 ]
