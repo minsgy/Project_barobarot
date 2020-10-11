486fecf0897cb5efb980355af156f88ced6eb89e
thalals <thalals97@gmail.com>
2020-10-12 02:20:29 +0900

    [hmin] payment 연결중

--- payments/urls.py
+++ payments/urls.py
@@ -1,3 +1,4 @@
+from os import name
 from django.urls import path
 from . import views
 
@@ -9,5 +10,6 @@ urlpatterns = [
     path('order_list/', views.PaymentproductList.as_view(), name="order_list"), # 주문 내역 리스트 페이지
     path('paymentproduct/create/<int:product_pk>/<int:user_pk>/', views.PaymentproductCreate, name="paymentproduct_create"), # 주문 아이템 생성
     path('paymentproduct/<int:pk>/',views.payment_do.as_view(), name = "payment_do"), # 주문 페이지 입장
-    path('order_success/<int:user_pk>/<int:payment_pk>/', views.order_success, name="order_success") # 주문 성공 페이지 입장
+    path('order_success/<int:user_pk>/<int:payment_pk>/', views.order_success, name="order_success"), # 주문 성공 페이지 입장
+    path('__payment_address_popup', views.popup, name="popup")
 ]
