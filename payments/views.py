5fe279ed72235e05a4a0491cee06f061b0c51fdf
thalals <thalals97@gmail.com>
2020-10-11 20:35:14 +0900

    [hmin] forms.py datetime 필드 값 저장받아오기

--- payments/views.py
+++ payments/views.py
@@ -14,6 +14,7 @@ from users import models as user_model
 from engineers import models as engineer_model
 from core import models as core_model
 
+from engineers.forms import MyForm  #엔지니어 폼(스케줄) - 시간단위 입력
 
 # [형민] reservation view
  
@@ -45,7 +46,10 @@ class payment_do(DetailView):
         context['amount'] = self.request.GET.get('amounts') # 전 페이지에서 수량 값 받아옴.
         context['paymentproducts'] = models.Paymentproduct
         context['engineers'] = engineer_model.Engineer.objects.all()        #모든 엔지니어 데이터 넘기기
-        return context
+        context['form'] = MyForm()        #시간단위 입력 폼
+        
+        
+        return context  
 
         
 # 결제 상품 생성
@@ -59,49 +63,56 @@ def PaymentproductCreate(request, product_pk, user_pk):
         user_data = user_model.User.objects.get(pk=user_pk)
         engineer_data = engineer_model.Engineer.objects.get(pk=(request.POST.get('engineer')))   #엔지니어 연결
 
-        receiver_data = Receiver()
-        receiver_data.receiver_name = request.POST.get('receiver_name')
-        receiver_data.receiver_number = request.POST.get('receiver_number')
-        receiver_data.receiver_plus_number = request.POST.get('receiver_number2')
-        receiver_data.zip_code = request.POST.get('zip_code')
-        receiver_data.address = request.POST.get('address')
-        receiver_data.delivery_message = request.POST.get('delivery_message')
+        form = MyForm(request.POST, request.FILES)      #form 파일로 데이터 받아오기
 
-        receiver_data.save()
+        if form.is_valid() :
 
-        print("엔지니어 pk : ",request.POST.get('engineer'))
-        paymentproducts.product = products_data
-        paymentproducts.user = user_data
-        paymentproducts.engineer = engineer_data
-        paymentproducts.receiver = receiver_data
+            print("확시 : ", form.cleaned_data['scheduled_time'])
+            # --------------------------------------------------------------------------
+            receiver_data = Receiver()
+            receiver_data.receiver_name = request.POST.get('receiver_name')
+            receiver_data.receiver_number = request.POST.get('receiver_number')
+            receiver_data.receiver_plus_number = request.POST.get('receiver_number2')
+            receiver_data.zip_code = request.POST.get('zip_code')
+            receiver_data.address = request.POST.get('address')
+            receiver_data.delivery_message = request.POST.get('delivery_message')
 
-        paymentproducts.amount = request.POST.get('amount') # 수량값 저장되게
-        paymentproducts.address = request.POST.get('address')
+            receiver_data.save()
 
-        # paymentproducts.created_time = core_models.TimeStampedModel.created
-        paymentproducts.visit_date = request.POST.get('visit_date')
-        paymentproducts.visit_time = request.POST.get('visit_time')
-        # amount 값과 물건 가격의 값을 곱해 합 가격을 구합니다.
-        # paymentproducts.total_price = paymentproducts.amount * paymentproducts.product.price 
-        
-        schedule = Schedule()
-        schedule.engineer = engineer_data
-        schedule.scheduled_date = request.POST.get('visit_date')
-        schedule.scheduled_time = request.POST.get('visit_time')
-        
-        schedule.save()
+            # --------------------------------------------------------------------------------------
+            print("엔지니어 pk : ",request.POST.get('engineer'))
+            paymentproducts.product = products_data
+            paymentproducts.user = user_data
+            paymentproducts.engineer = engineer_data
+            paymentproducts.receiver = receiver_data
 
-        # engineer_name = engineer_model.Engineer.objects.get(engineer_data)
+            paymentproducts.amount = request.POST.get('amount') # 수량값 저장되게
+            paymentproducts.address = request.POST.get('address')
 
-        # date = engineer_model.Schedule.objects.filter(engineer= engineer_name) #역참조
+            # paymentproducts.created_time = core_models.TimeStampedModel.created
+            paymentproducts.visit_date = request.POST.get('visit_date')
+            paymentproducts.visit_time = form.cleaned_data['scheduled_time']    #form fields 값 가져오기
 
-        # date = engineer_name.schedule.all()
+            # -------------------------------------------------------------------------------------
 
-        # date.scheduled_date = request.POST.get('visit')
+            # amount 값과 물건 가격의 값을 곱해 합 가격을 구합니다.
+            # paymentproducts.total_price = paymentproducts.amount * paymentproducts.product.price 
+            
+            schedule = Schedule()
+            schedule.engineer = engineer_data
+            schedule.scheduled_date = request.POST.get('visit_date')
+            schedule.scheduled_time = form.cleaned_data['scheduled_time']
+            
+            schedule.save()
 
-        paymentproducts.save()
-        return redirect('payments:order_success', user_pk, paymentproducts.pk )
-        # 생성 된 주문 아이템 pk, 로그인한 user_pk 가지고 가기
+            paymentproducts.save()
+
+            return redirect('payments:order_success', user_pk, paymentproducts.pk )
+            # 생성 된 주문 아이템 pk, 로그인한 user_pk 가지고 가기
+
+        else :
+            print("form none valided")
+
     return render(request, 'payments/payment_doing.html')
 
 
