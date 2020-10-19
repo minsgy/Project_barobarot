3239889b7e7617f8cb440316bc9b5e151bfce892
minsgy <alstjrchl97@naver.com>
2020-10-19 22:44:27 +0900

    [minseok] : products 모델 생성 뷰 제작

--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     # path('', views.getHome, name="home"),
     path('', views.HomeView.as_view(), name="home"),
     path('products/<int:pk>', views.ProductDetail.as_view(), name="product_detail"), # 상품상세url
+    path('products_test/', views.create_products, name="products_create"),
     # path('products/amount/<int:pk>', views.products_amount_get, name="product_amount"), # 상품상세url
 ]
